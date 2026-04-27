0aed0ebcb0222299d6adb17de1942b8e7950e4a8
Peter Lyons Kehl <peter.kehl@gmail.com>
2026-04-26 19:15:50 -0700

    Tests

--- negative_tests/src/lib.rs
+++ negative_tests/src/lib.rs
@@ -1,4 +1,11 @@
 #[cfg(not(debug_assertions))]
-compile_error!("Build in debug mode only. Otherwise, since we have multiple sequential parts \
+compile_error!(
+    "Build in debug mode only. Otherwise, since we have multiple sequential parts \
                 where each fails with panic!, a release build would optimize the subsequent \
-                panics out.");
+                panics out."
+);
+
+#[cfg(test)]
+compile_error!(
+    "Don't test. Instead, build (in debug, that is, with `cargo build` or `cargo build --debug`."
+);
--- positive_tests/src/lib.rs
+++ positive_tests/src/lib.rs
@@ -1,2 +1,5 @@
 #[cfg(debug_assertions)]
 compile_error!("Build in release mode only.");
+
+#[cfg(test)]
+compile_error!("Don't test. Instead, build (in release, that is, with `cargo build --release`.");
